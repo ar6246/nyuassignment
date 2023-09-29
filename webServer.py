e4349aee200057a9535a5b89689cf514eb04ed12
ar6246 <143233543+ar6246@users.noreply.github.com>
2023-09-29 13:30:39 -0400

    formatting

--- webServer.py
+++ webServer.py
@@ -38,7 +38,7 @@ def webServer(port=13331):
       outputdata = b"HTTP1.1/200 OK\r\n Content-Type: text/html; charset=UTF-8\r\n"
       #validrequest = b"HTTP1.1/200 OK\r\n"
 
-      connectionSocket.sendall(outputdata)
+      connectionSocket.send(outputdata)
 
       #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
  
@@ -63,7 +63,7 @@ def webServer(port=13331):
       # Remember the format you used in the try: block!
       #Fill in start
       invalidrequest = b"HTTP1.1/404 Not Found\r\nContent-Type: text/html; charset=UTF-8\r\n"
-      connectionSocket.sendall(invalidrequest)
+      connectionSocket.send(invalidrequest)
 
       #Fill in end
 
